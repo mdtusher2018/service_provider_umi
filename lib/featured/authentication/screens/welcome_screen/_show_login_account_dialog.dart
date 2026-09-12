019685c171d13f32017809bfc3266ea4ae0f71f2
siamprodhan <siambci@gmail.com>
2026-09-12 11:47:16 +0600

    12/09/26: Some issues has been fixed

--- lib/featured/authentication/screens/welcome_screen/_show_login_account_dialog.dart
+++ lib/featured/authentication/screens/welcome_screen/_show_login_account_dialog.dart
@@ -94,9 +94,9 @@ class _LoginDialogState extends ConsumerState<_LoginDialog> {
               Container(
                 padding: const EdgeInsets.all(12),
                 decoration: BoxDecoration(
-                  color: Colors.red.withOpacity(0.1),
+                  color: Colors.red.withValues(alpha: 0.1),
                   borderRadius: BorderRadius.circular(8),
-                  border: Border.all(color: Colors.red.withOpacity(0.3)),
+                  border: Border.all(color: Colors.red.withValues(alpha: 0.3)),
                 ),
                 child: Row(
                   children: [
